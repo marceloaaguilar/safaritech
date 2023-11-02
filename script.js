f7f3867476d2f8bddc753bb65d95800addb4a59d
Marcelo Aguilar <marcelodalmeida42@gmail.com>
2023-11-02 13:15:31 -0300

    New Updates, Swiper and Responsive fixes

---
+++ script.js
@@ -0,0 +1,13 @@
+var swiper = new Swiper(".swiper", {
+    loop: true,
+    autoplay: {
+        delay: 1,
+        disableOnInteraction: false
+    },
+    slidesPerView: '4',
+    spaceBetween: 50,
+    grabCursor: false,
+    freeMode: true,
+    speed: 5000,
+    
+  });
